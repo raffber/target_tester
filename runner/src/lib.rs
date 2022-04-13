58482f330860efa04433687bfa1d21433c380c82
Raphael Bernhard <beraphae@gmail.com>
2022-04-13 14:08:23 +0200

    Remove jlink api

--- runner/src/lib.rs
+++ runner/src/lib.rs
@@ -1,15 +1,8 @@
 use object::elf::FileHeader32;
 use object::Endianness;
 
-mod bindings;
 pub mod config;
-pub mod jlink;
 
-#[allow(non_upper_case_globals)]
-#[allow(non_camel_case_types)]
-#[allow(non_snake_case)]
-#[allow(dead_code)]
-pub mod jlink_sys;
 pub mod report;
 pub mod connection;
 pub mod runner;
